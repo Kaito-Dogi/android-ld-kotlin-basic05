69d2fb19a2e109b20eaf91c54580d6646268a64a
Kaito Dogi <kendobu0405@gmail.com>
2022-05-15 03:10:05 +0900

    feat: 答えの実装

--- app/src/main/java/leaders/android/kotlinbasic05/MainActivity.kt
+++ app/src/main/java/leaders/android/kotlinbasic05/MainActivity.kt
@@ -3,6 +3,7 @@ package leaders.android.kotlinbasic05
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import leaders.android.kotlinbasic05.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // くじを引くボタンを押した時の処理
+        binding.drawLotsButton.setOnClickListener {
+            val randomNum: Int = Random.nextInt(5)
+            binding.resultImage.setImageResource(getOmikujiResult(randomNum))
+        }
+    }
+
+    // おみくじの画像を返すメソッド
+    fun getOmikujiResult(randomNum: Int): Int {
+        // drawableリソースの配列を宣言
+        val omikujiImages: Array<Int> = arrayOf(
+            R.drawable.omikuji_daikichi,
+            R.drawable.omikuji_chuukichi,
+            R.drawable.omikuji_syoukichi,
+            R.drawable.omikuji_kyou,
+            R.drawable.omikuji_daikyou,
+        )
+        return omikujiImages[randomNum]
     }
 }
